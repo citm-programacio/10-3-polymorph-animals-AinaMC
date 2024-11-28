b71444bc7a99cc0af23859f362a60b120292ec5e
ainamc <aina.mestres@estudiantat.upc.edu>
2024-11-28 17:24:37 +0100

    Enviar a profe, ej  hecho

--- 10.3 Polymorph Animals.cpp
+++ 10.3 Polymorph Animals.cpp
@@ -2,18 +2,50 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void sonido()=0; //Permite que todos los SONIDO() empiezen asi
+   // {
+    //    cout << " El animal hace un sonido: ";
+    //}
+};
+
+class Perro : Animal
+{
+public:
+    void sonido() override
+    {
+        cout << "woof woof";
+    }
+};
+class Gato : Animal
+{
+public:
+    void sonido() override
+    {
+        cout << "miau";
+    }
+};
+class Pato : Animal
+{
+public:
+    void sonido() override
+    {
+        cout << "quack quack";
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Animal* animal1 = new Perro();
+    Gato* animal2 = new Gato();
+    Pato* animal3 = new Pato();
+
+    animal1->sonido();
+    animal2->sonido();
+    animal3->sonido();
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
